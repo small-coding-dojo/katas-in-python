748294ccd15bee09590531893fa82edc22566f49
Stefan Boos <kontakt@boos.systems>
2024-04-27 21:16:18 +0200

    refactor: single data driven test replaces multiple tests

--- tests/test_kyu6_bit_counting.py
+++ tests/test_kyu6_bit_counting.py
@@ -1,29 +1,16 @@
+import pytest
+
 from kyu6_bit_counting.count_bits import count_bits
 
 
-def test_count_bits():
-    assert count_bits(0) == 0
-
-
-def test_count_bits_():
-    assert count_bits(1) == 1
-
-
-def test_count_bits__():
-    assert count_bits(2) == 1
-
-
-def test_count_bits___():
-    assert count_bits(3) == 2
-
-
-def test_count_bits____():
-    assert count_bits(4) == 1
-
-
-def test_count_bits_____():
-    assert count_bits(5) == 2
-
-
-def test_count_bits______():
-    assert count_bits(7) == 3
+@pytest.mark.parametrize('value, expected', [
+    (0, 0),
+    (1, 1),
+    (2, 1),
+    (3, 2),
+    (4, 1),
+    (5, 2),
+    (7, 3),
+])
+def test_count_bits(value, expected):
+    assert count_bits(value) == expected
