b532ba1aae5732d08daa555a7cc736f2ba811271
Stefan Boos <kontakt@boos.systems>
2024-04-27 21:30:26 +0200

    refactor: simplify logic

--- src/kyu6_bit_counting/count_bits.py
+++ src/kyu6_bit_counting/count_bits.py
@@ -6,9 +6,10 @@ def count_bits(n):
         return 0
 
     highest_bit = floor(log(n, 2))
-    bits = range(highest_bit, -1, -1)
-    set_bits = [1 if n & 2 ** bit else 0 for bit in bits]
-    return sum(set_bits)
+    possible_bits = range(highest_bit, -1, -1)
+    binary_representation = [n & 2 ** bit > 0 for bit in possible_bits]
+
+    return binary_representation.count(True)
 
 
 if __name__ == "__main__":
