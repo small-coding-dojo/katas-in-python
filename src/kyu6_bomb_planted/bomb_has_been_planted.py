e0c6ae95931a673d21c2b3ac93722cbc0eee1dd9
Stefan Boos <kontakt@boos.systems>
2024-05-01 13:50:52 +0200

    refactor: simplify more by replacing coordinate with tuple

--- src/kyu6_bomb_planted/bomb_has_been_planted.py
+++ src/kyu6_bomb_planted/bomb_has_been_planted.py
@@ -8,39 +8,30 @@ TIME_TO_DEFUSE_WITHOUT_KIT = 10
 TIME_TO_DEFUSE_WITH_KIT = 5
 
 
-class Coordinate:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+def bomb_has_been_planted(m, time):
+    bomb, counter_terrorist, defuse_kit = None, None, None
+
+    # Locate bomb, counter terrorist and defuse kit
+    for y, row in enumerate(m):
+        if BOMB in row:
+            bomb = (row.index(BOMB), y)
+        if COUNTER_TERRORIST in row:
+            counter_terrorist = (row.index(COUNTER_TERRORIST), y)
+        if DEFUSE_KIT in row:
+            defuse_kit = (row.index(DEFUSE_KIT), y)
+
+    # Time needed without defuse kit
+    time_required = TIME_TO_DEFUSE_WITHOUT_KIT + distance(bomb, counter_terrorist)
+
+    # Time needed with defuse kit
+    if defuse_kit:
+        time_with_defuse_kit = (TIME_TO_DEFUSE_WITH_KIT
+                                + distance(bomb, defuse_kit)
+                                + distance(defuse_kit, counter_terrorist))
+        time_required = min(time_required, time_with_defuse_kit)
+
+    return time_required <= time
 
 
 def distance(a, b):
-    return max(abs(a.x - b.x), abs(a.y - b.y))
-
-
-def bomb_has_been_planted(m, time):
-    bomb = None
-    counter_terrorist = None
-    defuse_kit = None
-
-    # Locate the bomb, counter terrorist and defuse kit
-    for row_number, row in enumerate(m):
-        if BOMB in row:
-            bomb = Coordinate(row.index(BOMB), row_number)
-        if COUNTER_TERRORIST in row:
-            counter_terrorist = Coordinate(row.index(COUNTER_TERRORIST), row_number)
-        if DEFUSE_KIT in row:
-            defuse_kit = Coordinate(row.index(DEFUSE_KIT), row_number)
-
-    # Time needed without defuse kit
-    distance_bomb_counter_terrorist = distance(bomb, counter_terrorist)
-    time_without_defuse_kit = TIME_TO_DEFUSE_WITHOUT_KIT + distance_bomb_counter_terrorist
-
-    # Time needed with defuse kit
-    time_with_defuse_kit = time_without_defuse_kit
-    if defuse_kit is not None:
-        distance_bomb_defuse_kit = distance(bomb, defuse_kit)
-        distance_defuse_kit_counter_terrorist = distance(defuse_kit, counter_terrorist)
-        time_with_defuse_kit = TIME_TO_DEFUSE_WITH_KIT + distance_bomb_defuse_kit + distance_defuse_kit_counter_terrorist
-
-    return time_without_defuse_kit <= time or time_with_defuse_kit <= time
+    return max(abs(a[0] - b[0]), abs(a[1] - b[1]))
