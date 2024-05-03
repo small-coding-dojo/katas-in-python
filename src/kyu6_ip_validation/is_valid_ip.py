2e60d33e404c234dcaf4b58bf72345dfd26cdd62
Stefan Boos <kontakt@boos.systems>
2024-05-03 22:07:36 +0200

    refactor: use IPv4Address instead of regular expression

--- src/kyu6_ip_validation/is_valid_ip.py
+++ src/kyu6_ip_validation/is_valid_ip.py
@@ -1,20 +1,9 @@
-import re
+from ipaddress import IPv4Address
 
 
 def is_valid_IP(input):
-    print(f"'{input}'")
-    if input != input.strip():
+    try:
+        IPv4Address(input)
+        return True
+    except:
         return False
-
-    match = re.match("^(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})$", input)
-
-    if not match:
-        return False
-
-    groups = match.groups()
-    for number_string in groups:
-        number = int(number_string)
-        if number_string != str(number) or number < 0 or number > 255:
-            return False
-
-    return True
