0b877cc31da8c16ef15f559d2c983e3f0a703556
Falk Kühnel <falk@kuehnel.eu>
2024-05-23 16:57:24 +0200

    simple prime

--- src/kyu6_is_prime/is_prime.py
+++ src/kyu6_is_prime/is_prime.py
@@ -1,44 +1,98 @@
-def sieve_of_atkin(num):
-    """Sieve of Atkin IS TOO SLOW ...
+# Python 3 program for
+# implementation of
+# Sieve of Atkin
+import math
 
-    See: https://www.geeksforgeeks.org/sieve-of-atkin/
-    """
-    sieve = [False] * (num + 1)
-    if num >= 2:
-        sieve[2] = True
-    if num >= 3:
-        sieve[3] = True
 
+def SieveOfAtkin(limit):
+    # 2 and 3 are known
+    # to be prime
+    if limit > 2:
+        print(2, end=" ")
+    if limit > 3:
+        print(3, end=" ")
+
+    # Initialise the sieve
+    # array with False values
+    sieve = [False] * (limit + 1)
+    for i in range(0, limit + 1):
+        sieve[i] = False
+
+    '''Mark sieve[n] is True if
+    one of the following is True:
+    a) n = (4*x*x)+(y*y) has odd
+    number of solutions, i.e.,
+    there exist odd number of
+    distinct pairs (x, y) that
+    satisfy the equation and
+    n % 12 = 1 or n % 12 = 5.
+    b) n = (3*x*x)+(y*y) has
+    odd number of solutions
+    and n % 12 = 7
+    c) n = (3*x*x)-(y*y) has
+    odd number of solutions,
+    x > y and n % 12 = 11 '''
     x = 1
-    while x * x <= num:
+    while x * x <= limit:
         y = 1
-        while y * y <= num:
+        while y * y <= limit:
+
+            # Main part of
+            # Sieve of Atkin
             n = (4 * x * x) + (y * y)
-            if n <= num and (n % 12 == 1 or n % 12 == 5):
+            if (n <= limit and (n % 12 == 1 or
+                                n % 12 == 5)):
                 sieve[n] ^= True
 
             n = (3 * x * x) + (y * y)
-            if n <= num and n % 12 == 7:
+            if n <= limit and n % 12 == 7:
                 sieve[n] ^= True
 
             n = (3 * x * x) - (y * y)
-            if x > y and n <= num and n % 12 == 11:
+            if (x > y and n <= limit and
+                    n % 12 == 11):
                 sieve[n] ^= True
-
             y += 1
         x += 1
 
+    # Mark all multiples of
+    # squares as non-prime
     r = 5
-    while r * r <= num:
+    while r * r <= limit:
         if sieve[r]:
-            for i in range(r * r, num + 1, r * r):
+            for i in range(r * r, limit + 1, r * r):
                 sieve[i] = False
 
-    result = [i for i, value in enumerate(sieve) if value]
-    return result
+        r += 1
+
+    # Print primes
+    # using sieve[]
+    primes = [2,3]
+    for a in range(5, limit + 1):
+        if sieve[a]:
+            print(a, end=" ")
+            primes.append(a)
+
+    return primes
+
+# This code is contributed
+# by Smitha
+
+def sieve_of_atkin(num):
+    return SieveOfAtkin(num)
+
 
 
 def is_prime(num):
+    return is_prime_simple(num)
     print ("number: {}".format(num), flush=True)
     primes = sieve_of_atkin(num)
     return num in primes
+
+def is_prime_simple(num):
+    if num < 2:
+        return False
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0 :
+            return False
+    return True
--- tests/test_kyu6_is_prime.py
+++ tests/test_kyu6_is_prime.py
@@ -8,6 +8,6 @@ def test_not_prime(test_input):
     assert not is_prime(test_input)
 
 
-@pytest.mark.parametrize('test_input', [2, 3, 5, 7, 11, 5099])
+@pytest.mark.parametrize('test_input', [2, 3, 5, 7, 11, 5009, 12345653, 999999999989, 1000000000000279]) # 12345653
 def test_prime(test_input):
     assert is_prime(test_input)
