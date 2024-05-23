abf2644c3a9804e1483c07a801f0206498ec0896
Falk Kühnel <falk@kuehnel.eu>
2024-05-23 17:24:52 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:tests/test_kyu6_is_prime.py

--- tests/test_kyu6_is_prime.py
+++ tests/test_kyu6_is_prime.py
@@ -1,3 +1,6 @@
+import statistics
+import time
+
 import pytest
 
 from kyu6_is_prime.is_prime import is_prime
@@ -11,3 +14,18 @@ def test_not_prime(test_input):
 @pytest.mark.parametrize('test_input', [2, 3, 5, 7, 11, 5009, 12345653, 999999999989, 1000000000000279]) # 12345653
 def test_prime(test_input):
     assert is_prime(test_input)
+
+
+@pytest.mark.parametrize('test_input', [999999999989]) # 12345653
+def test_prime(test_input):
+    counter = 100
+    measurements = []
+    for i in range(1, counter):
+        t = time.process_time_ns()
+        assert is_prime(test_input)
+        delta = (time.process_time_ns() - t)/1000000
+        measurements.append(delta)
+    print (measurements)
+    print(f"median : {statistics.median(measurements)}ms")
+    print(f"min : {min(measurements)}ms")
+    print(f"max : {max(measurements)}ms")
